d953d71957213a8dfbf7dfc8636eec3273897f4f
Lahik <lahik65@gmail.com>
2025-04-05 20:37:54 +0530

    Pipe Animation

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ let birdHeight = 24;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
 
+//! pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+let pipeGap = boardHeight/6;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//! physics
+let velocityX = -2;
+
 let bird = {
     x : birdX,
     y : birdY,
@@ -32,4 +46,54 @@ window.onload = function() {
     birdImg.onload = function() {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "toppipe.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "bottompipe.png";
+
+    requestAnimationFrame(updateGraphics);
+
+    setInterval(placePipes, 1500);
+}
+
+function updateGraphics() {
+    requestAnimationFrame(updateGraphics);
+    context.clearRect(0, 0, boardWidth, boardHeight);
+
+    //! bird
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    //! pipes 
+    for(let i = 0; i < pipeArray.length; i++) {
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+}
+
+function placePipes() {
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random() * (pipeHeight / 2);
+
+    let topPipe = {
+        img : topPipeImg,
+        x : pipeX,
+        y : randomPipeY,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+    
+    let bottomPipe = {
+        img : bottomPipeImg,
+        x : pipeX,
+        y : randomPipeY + pipeHeight + pipeGap,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+
+    pipeArray.push(topPipe);
+    pipeArray.push(bottomPipe);
 }
